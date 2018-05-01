a7f5193958e7e668c7b2212296d36d8aea92898b
ypinhas <yaacovx.pinhas@gmail.com>
2018-05-01 11:08:02 +0300

    Added LRS installer for Windows OS

--- Windows/rs_autolabs_utils/LRS/libRealSenseInstaller.bat
+++ Windows/rs_autolabs_utils/LRS/libRealSenseInstaller.bat
@@ -12,12 +12,11 @@ if exist "C:\Program Files (x86)\Intel RealSense SDK 2.0\unins000.exe" (
 
 echo Atempting to install the new libRealSense SDK...
 "C:\temp\LRS\Intel.RealSense.SDK.exe" /Silent
-echo The new version of libRealSense installed successfully
 
-pause
-exit
-
-
-
-
-
+if exist "C:\Program Files (x86)\Intel RealSense SDK 2.0\tools\realsense-viewer.exe" (
+	echo The new version of libRealSense installed successfully
+	exit 0
+} else (
+	echo Failed to install the new version of libRealSense!
+	exit 1
+)
