ef0038ecf2e05507998a2deded8585addfa9c8c7
ygartix <37108127+ygartix@users.noreply.github.com>
2018-05-07 17:08:07 +0300

    Update Install.sh

--- Linux/rs_autolabs_utils/LRS/Install.sh
+++ Linux/rs_autolabs_utils/LRS/Install.sh
@@ -4,7 +4,11 @@
 function runcmd
 {
 	echo "running \"$1\""
-	$1
+	if [ "$2" = "sudo" ]; then
+		echo trio_012 | sudo -S $1
+	else
+		$1
+	fi
 	if [ $? -ne 0 ]; then
 		echo ERROR!
 		exit 1
@@ -12,9 +16,10 @@ function runcmd
 }
 
 
-runcmd "echo trio_012 | sudo -S  apt update"
-runcmd "echo trio_012 | sudo -S apt dist-upgrade"
-runcmd "echo trio_012 | sudo -S  apt install libssl-dev"
+runcmd "apt update" sudo
+runcmd "apt dist-upgrade" sudo
+runcmd "apt install libssl-dev" sudo
+runcmd "chown -R administrator:administrator /home/administrator/librealsense" sudo
 runcmd "cd /home/administrator/librealsense"
 if [ -d "build" ]; then
 	runcmd "rm -rf build"
@@ -22,12 +27,12 @@ fi
 runcmd "mkdir build"
 runcmd "cd build"
 runcmd "cmake ../ -DBUILD_EXAMPLES=TRUE  -DBUILD_PYTHON_BINDINGS=bool:true -DPYTHON_EXECUTABLE=/usr/bin/python2.7 -DCMAKE_BUILD_TYPE=release"
-runcmd "echo trio_012 | sudo -S make uninstall"
+runcmd "make uninstall" sudo
 runcmd "make clean"
 runcmd "make -j8"
-runcmd "echo trio_012 | sudo -S make install"
+runcmd "make install" sudo
 runcmd "cd .."
-runcmd "echo trio_012 | sudo -S cp config/99-realsense-libusb.rules /etc/udev/rules.d/"
-runcmd "echo trio_012 | sudo -S udevadm control --reload-rules"
-runcmd "echo trio_012 | sudo -S udevadm trigger"
-runcmd "echo trio_012 | sudo -S smodprobe uvcvideo"
+runcmd "cp config/99-realsense-libusb.rules /etc/udev/rules.d/" sudo
+runcmd "udevadm control --reload-rules" sudo
+runcmd "udevadm trigger" sudo
+runcmd "modprobe uvcvideo" sudo
